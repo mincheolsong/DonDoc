febaaf91a89ca63ad52c6b0ebf0c23b2a7d7affe
유영서 <you2882@naver.com>
2023-09-08 10:42:07 +0900

    Remove : All/NoArgsConstructor 삭제

--- bank/backend/src/main/java/com/bank/backend/dto/HistoryDto.java
+++ bank/backend/src/main/java/com/bank/backend/dto/HistoryDto.java
@@ -1,16 +1,12 @@
 package com.bank.backend.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 public class HistoryDto {
 
     @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class Request {
 
         @NotBlank(message = "식별번호를 입력해주세요.")
