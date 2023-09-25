ba85373edf84d2d6d7a8024339c3f5928c4be9cd
안영기 <thfl9281@naver.com>
2023-09-25 10:06:50 +0900

    Fix: 모임홈 에러로 예전 코드 업로드

---
+++ dondoc/frontend/src/pages/moim/MoimHome/Moimaaa.tsx
@@ -0,0 +1,125 @@
+// import styles from "./MoimHome.module.css";
+// // import haaland from '../../../assets/bbakbbakyee.jpg'
+// import peter from "../../../assets/image/peter.svg"
+// import Header from "../../webmain/Header/Header";
+// import chelsea from '../../../assets/Chelsea_FC_Logo.jpg'
+// import { useNavigate } from "react-router-dom";
+
+// function MoimHome() {
+
+//   const navigate = useNavigate()
+//   const ToCreateMoim = () => {
+//     navigate('/createmoim')
+//   }
+
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.content}>
+
+//         <Header />
+
+//         {/* <div className={styles.mytap}>
+//           <div className={styles.character}>
+//             <img src={haaland} alt="" className={styles.haaland}/>
+//           </div>
+//           <div className={styles.toprofile}>
+//             <p>동혁시치의 DonDoc</p>
+//             <button className={styles.goprofile}>내 프로필</button>
+//           </div>
+//           <div className={styles.makemoim}>
+//             <button className={styles.createmoimbtn} onClick={ToCreateMoim}>모임 생성</button>
+//           </div>
+//         </div> */}
+        
+//         <UserBox userCharacter={peter} username="Jaden" rightBtn="모임 생성"/>
+
+//         <div className={styles.moimlist}>
+//           <div className={styles.moimlisttitle}>
+//             <h1>나의 모임</h1>
+//           </div>
+
+//           <div className={styles.moimcontent}>
+//             <h1 className={styles.moimunit}>캐러셀해라 노예야 노예야</h1>
+//             <h1 className={styles.moimunit}>캐러셀해라 노예야 노예야</h1>
+//             <h1 className={styles.moimunit}>캐러셀해라 노예야 노예야</h1>
+//           </div>
+
+//         <div className={styles.mytap}>
+//           <div className={styles.character}>
+//             <img src={haaland} alt="" className={styles.haaland}/>
+//           </div>
+//           <div className={styles.toprofile}>
+//             <p>동혁시치의 DonDoc</p>
+//             <button className={styles.goprofile}>내 프로필</button>
+//           </div>
+//           <div className={styles.makemoim}>
+//             <button className={styles.createmoimbtn} onClick={ToCreateMoim}>모임 생성</button>
+//           </div>
+//         </div>
+
+//         <div className={styles.moimlist}>
+//           <div className={styles.moimlisttitle}>
+//             <h1>나의 모임</h1>
+//           </div>
+
+//           <div className={styles.moimcontent}>
+//             <h1 className={styles.moimunit}>캐러셀해라 노예야 노예야</h1>
+//             <h1 className={styles.moimunit}>캐러셀해라 노예야 노예야</h1>
+//             <h1 className={styles.moimunit}>캐러셀해라 노예야 노예야</h1>
+//           </div>
+
+//         </div>
+
+//         <div className={styles.invitelist}>
+//           <div className={styles.inviteisttitle}>
+//             <h1>초대현황</h1>
+//           </div>
+
+//           <div className={styles.invitecontent}>
+//             <div className={styles.invitebox}>
+//               <div className={styles.inviteunit}>
+//                 <div className={styles.linkbank}>
+//                 <img src={chelsea} className={styles.chelsea} alt="" />
+//                 </div>
+//                 <h4 className={styles.invitemessage}>아오아오주연주연시치가 아오동혁시치시치를 행복한 첼시에 초대하였습니다.</h4>
+//               </div>
+//             </div>
+//           </div>
+
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MoimHome;
+
+
+// export function UserBox(props){
+//   const navigate = useNavigate()
+//   const ToCreateMoim = () => {
+//     navigate('/createmoim')
+//   }
+
+//   return(
+//     <div className={styles.topContainer}>
+//       <div style={{display:"flex",width:"60%"}}>
+//         <img src={props.userCharacter} style={{width:"35%"}} />
+//         <div style={{marginLeft:"1rem",textAlign:"center"}}>
+//           <p style={{fontSize:"1.2rem",fontWeight:"bold"}}>{props.username} 의 DonDoc</p>
+//           <button className={styles.myProfileBtn} onClick={()=>{
+//             navigate("/mypage")
+//             }}> 나의프로필가기</button>
+//         </div>
+//       </div>
+      
+//       <div>
+//         <button className={styles.myProfileBtn} style={{height:"5rem",fontSize:"1.2rem"}} onClick={ToCreateMoim} > {props.rightBtn}</button>
+//       </div>
+//     </div>
+
+//   )
+
+// }
