e74edb873972357a33003b441aecf1ea4f3dd563
김동혁 <fhg4561@naver.com>
2023-10-05 16:24:42 +0900

    Feat : 미션 추가

---
+++ dondoc/frontend/src/pages/mission/Mission/Detail/Confirm/Confirm.tsx
@@ -0,0 +1,76 @@
+import styles from "./Confirm.module.css";
+import { BASE_URL } from "../../../../../constants";
+import axios from "axios";
+import { UserType } from "../../../../../store/slice/userSlice";
+import { useSelector } from "react-redux";
+
+type Missions = {
+  id: number,
+  moimName: string,
+  title: string,
+  content: string,
+  amount: number,
+  endDate: string,
+  moimId:number,
+}
+
+
+type Props = {
+  setOpenConfirm(id:boolean) : void,
+  Mi : Missions
+}
+
+
+function Confirm({setOpenConfirm, Mi}:Props) {
+
+  const CloseModal = () => {
+    setOpenConfirm(false)
+  }
+
+  const userInfo:UserType = useSelector((state:{user:UserType})=>{
+    return state.user
+  })
+
+  const token = userInfo.accessToken
+
+  const data = {
+    moimId: Mi.moimId,
+    requestId: Mi.id
+  }
+
+const GiveUpMission = () => {
+  axios.post(`${BASE_URL}/api/moim/quit_mission`,data,{
+    headers:{
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then((res) => {
+    console.log(res.data)
+    alert(res.data.response.msg)
+    location.reload()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+  return (
+
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.maincontent}>
+          <p style={{textAlign:"center", lineHeight:"15rem", fontSize:"2rem", fontWeight:"bold"}}>정말 포기하시겠습니까?</p>
+          
+        </div>
+          <div className={styles.btns}>
+            <button onClick={GiveUpMission} className={styles.refusebtn}>거절하기</button>
+            <button onClick={CloseModal} className={styles.acceptbtn}>닫기</button>
+          </div>
+        
+      </div>
+    </div>
+  )
+
+}
+
+export default Confirm
